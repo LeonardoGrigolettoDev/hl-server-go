18243a5cc5c3f76528426b19fb269e772fa2b13c
LeonardoGrigolettoDev <leonardochawder@gmail.com>
2024-10-08 17:19:58 -0300

    1st commit

---
+++ database/postgre/connection.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"fmt"
+	"database/sql"
+	"github.com/github.com/LeonardoGrigolettoDev/fly-esp-server-go/config"
+	_ "github.com/lib/pq"
+)
+
+func OpenConnection() (*sql.DB, error){
+	conf := config.GetDB()
+	sc := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disabled"
+	conf.Host, conf.Port, conf.User, conf.Pass, conf.Database)
+
+	conn, err := sql.Open("postgres", sc)
+	if err != nil {
+		panic(err)
+	}
+
+	err = conn.Ping()
+	return conn, err
+}
