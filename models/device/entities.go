4686581d9722644f93c578808fa657bde59c700a
LeonardoGrigolettoDev <leonardochawder@gmail.com>
2024-10-18 00:47:42 -0300

    Implementado a lógica de migração do DB para desenvolvimento (goose)

--- models/device/entities.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Device struct {
-	ID         int64  `json:"id"`
-	MAC        string `json:"mac"`
-	Actionable bool   `json:"actionable"`
-	Camera     bool   `json:"camera"`
-	Drone      bool   `json:"drone"`
-}
